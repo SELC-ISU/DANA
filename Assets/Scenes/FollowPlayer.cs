77c6415c71ca45d3654c629b3eed285a206d2ef1
David <harmon.david343@gmail.com>
2020-02-08 00:16:53 -0600

    Adding scene with physics, player control, and camera following

---
+++ Assets/Scenes/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    public Vector2 offset = new Vector2(0, 0);
+
+    private Vector3 relativePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        relativePosition = new Vector3(offset.x, offset.y, -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + relativePosition;
+    }
+}
---
+++ Assets/Scenes/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+
+    private float horizontal;
+    private bool jumpPressed;
+    private bool inAir;
+
+    public float speed = 0.0f;
+    public float jumpPower = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetButton("Jump")) {
+            jumpPressed = true;
+        }
+        if (inAir) {
+            jumpPressed = false;
+        }
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        if (jumpPressed && !inAir) {
+            Jump();
+            inAir = true;
+        }
+    }
+
+    private void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) {
+        inAir = false;
+    }
+}
