40419f755eb9546ea6096b1946b514c6630a0546
Danieljrand <danieljr@iastate.edu>
2020-02-18 10:55:46 -0600

    powerup1 script added

---
+++ Assets/Scenes/Daniel_Powerup1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daniel_Powerup1 : MonoBehaviour
+{
+    GameObject Circle, Box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Circle = GameObject.Find("Player_Circle");
+        Box = GameObject.Find("Player");
+
+        //start as Box
+        Box.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D entity)
+    {
+        //changes level if it player touches it
+        if (entity.gameObject.tag == "Player")
+                if (Circle.active == true)
+                {
+                    Box.transform.position = Circle.transform.position;
+                    Circle.SetActive(false);
+                    Box.SetActive(true);
+                }
+                else if (Box.active == true)
+                {
+                    Circle.transform.position = Box.transform.position;
+                    Box.SetActive(false);
+                    Circle.SetActive(true);
+                }   
+    }
+}
