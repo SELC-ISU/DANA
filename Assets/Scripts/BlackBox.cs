0f5731e883cf778917493a4be6e8919337efce95
David <harmon.david343@gmail.com>
2020-02-18 10:54:43 -0600

    created test

--- Assets/Scripts/BlackBox.cs
+++ Assets/Scripts/BlackBox.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class BlackBox : MonoBehaviour
 {
+    // The angle of this shifter that the gravity will shift to
+    public Vector3 rotation = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // Rotation variables to keep track of the relative direction
+    private float rot;
+    private float rotX;
+    private float rotY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initializes the radian rotation and directions
+        rot = (float) (rotation.z * Mathf.PI / 180f);
+        rotX = (float) Mathf.Sin(rot);
+        rotY = (float) Mathf.Cos(rot);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D entity) {
+        // Debug.Log(rot + "=" + rotX + "," + rotY);
+        // Updates the current world gravity angle to this gravity shifter
+        Gravity_Shift.angle = 180 - rotation.z;
+        // Updates the gravity to this gravity shifter
+        Physics2D.gravity = new Vector2(rotX * 9.81f, rotY * 9.81f);
     }
 }
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject projectile;
+    private Vector2 target;
+    private Vector2 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = new Vector2(0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            target = Camera.main.ScreenToWorldPoint((Vector2)Input.mousePosition);
+            Debug.Log("Target: " + target);
+            Instantiate(projectile, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+}
