d0ce06be9948df7ca002dd7c12ba6a48b103f2c2
David <harmon.david343@gmail.com>
2020-02-25 10:48:12 -0600

    Worked more on gravity wall

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
 
-    public GameObject projectile;
+    public GameObject projectilePrefab;
+    private GameObject projectile;
+    // private Projectile script;
+
     private Vector2 target;
     private Vector2 pos;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         target = new Vector2(0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
+            pos = rb.position;
             target = Camera.main.ScreenToWorldPoint((Vector2)Input.mousePosition);
+            float velX = target.x - pos.x;
+            float velY = target.y - pos.y;
+            Vector2 vel = new Vector2(velX, velY);
+
             Debug.Log("Target: " + target);
-            Instantiate(projectile, new Vector3(0, 0, 0), Quaternion.identity);
+
+            projectile = (GameObject) Instantiate(projectilePrefab, pos, Quaternion.identity);
+
+            ScriptProjectile script = (ScriptProjectile) projectile.GetComponent<ScriptProjectile>();
+            // Debug.Log(script);
+            script.setVel(velX, velY);
         }
     }
 }
