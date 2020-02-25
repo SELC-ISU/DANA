d1e52c9062bb9d71babf3971337187fa75ce0988
Danieljrand <danieljr@iastate.edu>
2020-02-25 10:52:24 -0600

    added teleporter stuff, but not quite yet working

---
+++ Assets/Scripts/Teleporter_1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter_1 : MonoBehaviour
+{
+    GameObject tp1, tp2, player;
+    Vector3 p1, p2;
+    // Start is called before the first frame update
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+    }
+    void Start()
+    {
+        tp1 = GameObject.Find("Teleporter");
+        tp2 = GameObject.Find("TP2");
+        player = GameObject.FindWithTag("Player");
+        p1 = tp1.transform.position;
+        p2 = tp2.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    //if player touches it
+    void OnTriggerEnter2D(Collider2D entity)
+    {
+        if(entity.gameObject == player)
+        {
+            player.transform.position = p2;
+            StartCoroutine(ExecuteAfterTime(1000));
+        }
+    }
+}
---
+++ Assets/Scripts/Teleporter_2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter_2 : MonoBehaviour
+{
+    GameObject tp1, tp2, player;
+    Vector3 p1, p2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tp1 = GameObject.Find("Teleporter");
+        tp2 = GameObject.Find("TP2");
+        player = GameObject.FindWithTag("Player");
+        p1 = tp1.transform.position;
+        p2 = tp2.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //if player touches it
+    void OnTriggerEnter2D(Collider2D entity)
+    {
+        if (entity.gameObject == player)
+        {
+            player.transform.position = p1;
+        }
+    }
+}
