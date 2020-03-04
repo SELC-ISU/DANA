1287962749471e0d42093da5e3a58efbd77e516c
Danieljrand <danieljr@iastate.edu>
2020-03-04 10:20:11 -0600

    Added target box

--- Assets/Scripts/nonplayer_movement.cs
+++ Assets/Scripts/nonplayer_movement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class nonplayer_movement : MonoBehaviour
 {
     private Vector3 startPos;
-    public Transform target;
-    public float speed;
+    public Transform target; //this is an block selected that the platform will move to
+    public float speed; //inputted speed
     private bool moveUp;
     void Start()
     {
@@ -15,18 +15,22 @@ public class nonplayer_movement : MonoBehaviour
     void Update()
     {
         float step = speed * Time.deltaTime;
+        //if the block has reached its target
         if (transform.position == target.position)
         {
             moveUp = false;
         }
+        //if the block is at its starting position
         else if (transform.position == startPos)
         {
             moveUp = true;
         }
+        //move back towards initial position
         if (moveUp == false)
         {
             transform.position = Vector3.MoveTowards(transform.position, startPos, step);
         }
+        //move towards the targeted block
         else if (moveUp)
         {
             transform.position = Vector3.MoveTowards(transform.position, target.position, step);
