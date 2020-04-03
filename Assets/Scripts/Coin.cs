32c249cf2e94246d566b014eea3b3a1bf8770a67
David <harmon.david343@gmail.com>
2020-04-03 15:50:25 -0500

    Fixed Coin bug

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -9,15 +9,15 @@ public class Coin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    void onTriggerEnter2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.tag == "Player")
         {
