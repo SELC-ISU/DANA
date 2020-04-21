1d5b27beec9681e23e018235928fd6a9c868caff
David <harmon.david343@gmail.com>
2020-04-21 00:34:43 -0500

    Cleaned up some of the button and door code. Added ability to have buttons open multiple doors. Added animation to doors. I'm pretty sure I fixed each door-level I broke, but make sure to double check. Level 10 seems to be broken and I can't test it

---
+++ Assets/Scripts/BrownBox.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrownBox : MonoBehaviour
+{
+
+    private float height;
+
+    private float goalHeight;
+
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+        goalHeight = height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate() {
+        float currentWidth = transform.localScale.x;
+        float currentHeight = transform.localScale.y;
+
+        if (Mathf.Abs(goalHeight - currentHeight) < speed) {
+            transform.localScale = new Vector2(currentWidth, goalHeight);
+        } else if (currentHeight < goalHeight) {
+                transform.localScale = new Vector2(currentWidth, currentHeight + speed);
+        } else if (currentHeight > goalHeight) {
+                transform.localScale = new Vector2(currentWidth, currentHeight - speed);
+        }
+    }
+
+    public void Open() {
+        goalHeight = 0;
+    }
+
+    public void Close() {
+        goalHeight = height;
+    }
+}
--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -5,48 +5,36 @@ using UnityEngine;
 public class ButtonBehaviour : MonoBehaviour
 {
 
+    // public GameObject player;
+    public GameObject[] doors;
 
-    
-    public Collider2D Player;
-    public Collider2D door;
-    public float width;
-    public float height;
-    public float posx;
-    public float posy;
     // Start is called before the first frame update
     void Start()
     {
-        width = door.gameObject.transform.localScale.x;
-        height = door.gameObject.transform.localScale.y;
-        posx = door.gameObject.transform.position.x;
-        posy = door.gameObject.transform.position.y;
-    }       
+
+    }
 
     // Update is called once per frame
     void Update()
     {
 
     }
+
     void OnTriggerEnter2D(Collider2D entity)
     {
-        if (entity.gameObject.tag == "Player" || entity.gameObject.tag == "ActivatorBox")
-        {
-           
-           
-            door.gameObject.transform.localScale = new Vector2(1, 0);
-            door.gameObject.transform.position = new Vector3(door.gameObject.transform.position.x, 0);
-            
+        if (entity.gameObject.tag == "Player" || entity.gameObject.tag == "ActivatorBox") {
+            foreach (GameObject door in doors) {
+                door.GetComponent<BrownBox>().Open();
+            }
         }
-        
+
     }
     void OnTriggerExit2D(Collider2D entity)
     {
-        if (entity.gameObject.tag == "Player" || entity.gameObject.tag == "ActivatorBox")
-        {
-            //Physics2D.IgnoreCollision(door, Player);
-            door.gameObject.transform.localScale = new Vector2(width,height);
-            door.gameObject.transform.position = new Vector3(posx, posy);
-
+        if (entity.gameObject.tag == "Player" || entity.gameObject.tag == "ActivatorBox") {
+            foreach (GameObject door in doors) {
+                door.GetComponent<BrownBox>().Close();
+            }
         }
     }
 }
