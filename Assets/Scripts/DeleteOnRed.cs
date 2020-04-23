de9eb736ac5407a2c67b84fae12334ae0f212dea
David <harmon.david343@gmail.com>
2020-04-23 12:55:08 -0500

    Start to boss level

---
+++ Assets/Scripts/DeleteOnRed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteOnRed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) {
+        //Reloads level if it touches a RedBox
+        if(col.gameObject.tag == "RedBox") {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/RaiseToPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseToPoint : MonoBehaviour
+{
+
+    public float goalHeight = 0.0f;
+    public float speed = 0.1f;
+
+    private float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate() {
+        float currentX = transform.position.x;
+        float currentY = transform.position.y;
+
+        if (Mathf.Abs(goalHeight - currentY) < speed) {
+            transform.position = new Vector2(currentX, goalHeight);
+        } else if (currentY < goalHeight) {
+                transform.position = new Vector2(currentX, currentY + speed);
+        } else if (currentY > goalHeight) {
+                transform.position = new Vector2(currentX, currentY - speed);
+        }
+    }
+}
